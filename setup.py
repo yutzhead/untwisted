b34b28c4f1a37b96d3b9b19abfa63cbb7ba54af5
Cogito, sed non sum <cogito@sednonsum.com>
2015-09-04 03:25:57 +0200

    Ported to python3.

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
 #! /usr/bin/env python
 
 from distutils.core import setup
-setup(name="untwisted",
+
+try:
+   from distutils.command.build_py import build_py_2to3 \
+        as build_py
+except ImportError:
+   from distutils.command.build_py import build_py
+
+setup(name="vy",
       version="0.1",
-      packages=["untwisted", 
-                "untwisted.utils"],
+      packages=["vyapp", 
+                "vyapp.plugins",
+                "vyapp.plugins.syntax",
+                "vyapp.plugins.omen",             
+                "vyapp.plugins.syntax.themes",
+		"vyapp.plugins.jdb",
+		"vyapp.plugins.pdb"],
+      #package_dir={'vyapp':'vyapp'},
+      scripts=['vy'],
+      package_data={'vyapp': ['vyrc', '/vyapp/vyrc']},
       author="Iury O. G. Figueiredo",
-      author_email="ioliveira@id.uff.br")
-
-
+      author_email="ioliveira@id.uff.br",
+      cmdclass = {'build_py': build_py}
+)
